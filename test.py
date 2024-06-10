8b5dd85e43264b2481cdac8fd60a229123195424
jamedrano <jamedrano@uvg.edu.gt>
2024-06-10 17:40:28 -0600

    Update test.py

--- test.py
+++ test.py
@@ -14,6 +14,14 @@ from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
 import sklearn.metrics as mt
 
+@st.cache_data(experimental_allow_widgets=True)
+def load_data(uploaded_file,sh,h):
+ data = pd.read_excel(uploaded_file,header=h,sheet_name=sh,engine='openpyxl')
+ data.columns = data.columns.str.strip()
+  for col in data.columns:
+   if data[col].dtype == 'O':
+    data[col] = data[col].str.strip()    
+ return data
  
 
 st.set_page_config(page_title='Modelo Predictivo Resistencia a la Compresión CEMPRO', page_icon=None, layout="wide")
@@ -26,14 +34,7 @@ uploaded_file = st.sidebar.file_uploader("*Upload file here*")
 if uploaded_file is not None:
   sh = st.sidebar.selectbox("*Que hoja contiene los datos?*",pd.ExcelFile(uploaded_file).sheet_names)
   h = st.sidebar.number_input("*Que fila contiene los nombres de columnas?*",0,100)
-  @st.cache_data(experimental_allow_widgets=True)
-  def load_data(uploaded_file,sh,h):
-    data = pd.read_excel(uploaded_file,header=h,sheet_name=sh,engine='openpyxl')
-    data.columns = data.columns.str.strip()
-    for col in data.columns:
-      if data[col].dtype == 'O':
-        data[col] = data[col].str.strip()    
-    return data
+  
   data = load_data(uploaded_file,sh,h)
  
   
