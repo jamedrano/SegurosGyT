87d7f2fdb89bee50ea58565c4e18d77ba2b30cef
jamedrano <jamedrano@uvg.edu.gt>
2024-06-10 17:46:22 -0600

    Update test.py

--- test.py
+++ test.py
@@ -25,17 +25,17 @@ def load_data(uploaded_file,sh,h):
 
 
 @st.cache_data(experimental_allow_widgets=True)
- def to_excel(df):
-  output = BytesIO()
-  writer = pd.ExcelWriter(output, engine='xlsxwriter')
-  df.to_excel(writer, index=False, sheet_name='Sheet1')
-  workbook = writer.book
-  worksheet = writer.sheets['Sheet1']
-  format1 = workbook.add_format({'num_format': '0.00'}) 
-  worksheet.set_column('A:A', None, format1)  
-  writer.close()
-  processed_data = output.getvalue()
-  return processed_data
+def to_excel(df):
+ output = BytesIO()
+ writer = pd.ExcelWriter(output, engine='xlsxwriter')
+ df.to_excel(writer, index=False, sheet_name='Sheet1')
+ workbook = writer.book
+ worksheet = writer.sheets['Sheet1']
+ format1 = workbook.add_format({'num_format': '0.00'}) 
+ worksheet.set_column('A:A', None, format1)  
+ writer.close()
+ processed_data = output.getvalue()
+ return processed_data
 
 
 st.set_page_config(page_title='Modelo Predictivo Resistencia a la Compresión CEMPRO', page_icon=None, layout="wide")
