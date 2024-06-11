8408d8685e4bce3037e864dd5e79998790275d58
jamedrano <jamedrano@uvg.edu.gt>
2024-06-10 19:34:31 -0600

    Update test.py

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
 import sklearn.metrics as mt
 
-
+@st.cache
 def load_data(uploaded_file,sh,h):
  data = pd.read_excel(uploaded_file,header=h,sheet_name=sh,engine='openpyxl')
  data.columns = data.columns.str.strip()
@@ -156,19 +156,7 @@ if uploaded_file is not None:
     desplegar()
    
   with tab5:
-   @st.cache_data(experimental_allow_widgets=True)
-   def to_excel(df):
-    output = BytesIO()
-    writer = pd.ExcelWriter(output, engine='xlsxwriter')
-    df.to_excel(writer, index=False, sheet_name='Sheet1')
-    workbook = writer.book
-    worksheet = writer.sheets['Sheet1']
-    format1 = workbook.add_format({'num_format': '0.00'}) 
-    worksheet.set_column('A:A', None, format1)  
-    writer.close()
-    processed_data = output.getvalue()
-    return processed_data
-    
+       
    df_xlsx = to_excel(subdatos2)
    st.download_button(label='📥 Download Current Result',data=df_xlsx ,file_name= 'df_test.xlsx')
    
