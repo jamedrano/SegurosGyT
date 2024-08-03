da3b9694b0e46eb78ced72ebbd38fff0b28804dd
jamedrano <jamedrano@uvg.edu.gt>
2024-08-03 16:31:46 -0600

    Update test.py

--- test.py
+++ test.py
@@ -64,13 +64,13 @@ def desplegar():
  subset1 = subdatos2.drop(quitar, axis=1)
 
  impo_df = impo_df.sort_values('importance', ascending=False)
- y_pos = np.arange(len(impo_df.shape[1]))
+ # y_pos = np.arange(len(impo_df.shape[1]))
  
  fig2, axs2 = plt.subplots(1,2)
  fig2.set_size_inches(6,6)
  axs2[0,0].scatter(y, pred)
- axs2[0,1].barh(y_pos,impo_df['importance'])
- axs2[0,1].set_yticks(y_pos,impo_df['feature'])
+ # axs2[0,1].barh(y_pos,impo_df['importance'])
+ # axs2[0,1].set_yticks(y_pos,impo_df['feature'])
  st.pyplot(fig2)
  
 
