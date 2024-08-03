2907f59f402e980227d0347cd8bd64adc88cf449
jamedrano <jamedrano@uvg.edu.gt>
2024-08-03 17:59:33 -0600

    Update test.py

--- test.py
+++ test.py
@@ -71,7 +71,7 @@ def desplegar():
  fig2, (ax1,ax2) = plt.subplots(2)
  fig2.set_size_inches(6,6)
  ax1.scatter(y, pred)
- ax2.barh(y_pos,impo_df['importance'])
+ ax2.barh(impo_df.index,impo_df['importance'])
  # ax2.set_yticks(y_pos,impo_df.index)
  st.pyplot(fig2)
 
