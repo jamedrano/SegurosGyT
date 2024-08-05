76d180403298886eb346327fadea26921faf277f
jamedrano <jamedrano@uvg.edu.gt>
2024-08-04 18:03:31 -0600

    Update test.py

--- test.py
+++ test.py
@@ -209,29 +209,7 @@ if uploaded_file is not None:
     respuesta = 'R28D'
     desplegar()
    
-  """with tab5:
-   archivoModelo = st.file_uploader("Cargar Modelo")
-   
-
-   if archivoModelo is not None:
-    modeloprod = load_model(archivoModelo)
-    st.write("Model loaded")
-    st.write(modeloprod)
-    
-    datosprod = st.file_uploader("Cargar Datos Prod")
-    if datosprod is not None:     
-     datospred = load_data(datosprod, 'Sheet1', 0) 
-     
-     st.write("Predicting...")  
-     ypred = modeloprod.get_booster().predict(xgb.DMatrix(datospred))
-     ypred2 = pd.DataFrame({'ypred':ypred})
-     resultados = pegar(datospred, ypred2)
-     st.dataframe(resultados)
-     resulta2 = to_excel(resultados)
-     st.download_button(label='📥Descargar resultados',data=resulta2 ,file_name= 'resultados.xlsx')"""
-   
-
-   
+  
   
     
  
